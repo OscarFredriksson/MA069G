06e6f0c2aa538204b97e7f044ba29f08ea9a28ef
Oscar <osfr1701@student.miun.se>
2019-09-13 15:23:50 +0200

    Uppgift 3 klar

--- Labb1/Labb1_u2.m
+++ Labb1/Labb1_u2.m
@@ -2,9 +2,9 @@ format long;
 
 syms f(x)
 
-f(x) = (sqrt((x-5)^5)+2*cos(pi*sqrt(x)))/(sqrt(x+4*log(x-pi)) - 1)  % anger f(x)
+f(x) = (sqrt((x-5)^5)+2*cos(pi*sqrt(x)))/(sqrt(x+4*log(x-pi)) - 1);  % anger f(x)
 
-f_prim = diff(f) % anger ett uttryck för derivatan f?(x)
+f_prim = diff(f); % anger ett uttryck för derivatan f?(x)
 
 x = 7; % punkten x = x_0
 
